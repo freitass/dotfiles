e889ccdb786a0cc7c049ca14eb498deb81e3c998
Leandro Freitas <llf@certi.org.br>
2015-01-15 11:29:33 -0200

    Added new bashrc module

---
+++ bashrc.d/004-server-settings.sh
@@ -0,0 +1,4 @@
+export LC_ALL=C
+export ANDROID_SDK=/opt/android_tools/android-sdk-linux
+#export ANDROID_NDK=/opt/android_tools/android-ndk-r10
+export ANDROID_NDK=/opt/android_tools/android-ndk-r10c
