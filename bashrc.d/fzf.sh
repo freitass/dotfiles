a9fa9bf31e795717085d48ac7ced5e5cba20239e
Leandro Freitas <freitass@gmail.com>
2015-10-13 13:57:42 -0300

    Optimize FZF for git

--- bashrc.d/fzf.sh
+++ bashrc.d/fzf.sh
@@ -1,1 +1,3 @@
 export FZF_DEFAULT_OPTS="--extended --cycle"
+export FZF_DEFAULT_COMMAND='(git ls-tree -r --name-only HEAD ||
+  find * -name ".*" -prune -o -type f -print -o -type l -print) 2> /dev/null'
